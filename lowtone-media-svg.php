46c601830f48b34120594204896718807a19470e
Paul van der Meijs <paul@lowtone.nl>
2013-06-28 21:07:41 +0200

    First commit.

---
+++ lowtone-media-svg.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * Plugin Name: SVG
+ * Plugin URI: http://wordpress.lowtone.nl/media-svg
+ * Description: Use Scalable Vector Graphics as WordPress Media.
+ * Version: 1.0
+ * Author: Lowtone <info@lowtone.nl>
+ * Author URI: http://lowtone.nl
+ * License: http://wordpress.lowtone.nl/license
+ */
+/**
+ * @author Paul van der Meijs <code@lowtone.nl>
+ * @copyright Copyright (c) 2011-2012, Paul van der Meijs
+ * @license http://wordpress.lowtone.nl/license/
+ * @version 1.0
+ * @package wordpress\plugins\lowtone\media\video
+ */
+
+namespace lowtone\media\svg {
+
+	use lowtone\content\packages\Package;
+
+	// Includes
+	
+	if (!include_once WP_PLUGIN_DIR . "/lowtone-content/lowtone-content.php") 
+		return trigger_error("Lowtone Content plugin is required", E_USER_ERROR) && false;
+
+	$__i = Package::init(array(
+			Package::INIT_MERGED_PATH => __NAMESPACE__,
+		));
+
